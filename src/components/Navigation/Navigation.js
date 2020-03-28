7752476c824efe1252821e58448ea13ff5bc61a0
Matt Kim <mattsunwoongkim@gmail.com>
2020-03-27 23:27:42 -0700

    minor design change

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Navigation = () => {
   return (
-    <nav className='ma3 mt0' style={{display: 'flex', justifyContent: 'flex-end'}}>
-      <p className="f3 dim black underline pa2 pointer">Sign Out</p>
+    <nav className='ma4 mt0' style={{display: 'flex', justifyContent: 'flex-end'}}>
+      <p className="f4 dim black pa2 pointer">Sign Out</p>
     </nav>
   );
 }
